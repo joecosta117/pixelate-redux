b1da6367d2a76a07d2bc392b4c07d8800fc31001
jstoobz <stephensja7@gmail.com>
2018-10-10 12:44:40 -0400

    added store subscribe/unsubscribe

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import store from './store';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
     return (
       <div id="pixelate">
--- src/store.js
+++ src/store.js
@@ -17,12 +17,12 @@ const initialState = {};
 /* we'll also add the corresponding action creators */
 
 // And we'll revisit this reducer.
-function reducer(state = initialState, action) {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
   }
-}
+};
 
 const store = createStore(reducer, applyMiddleware(loggerMiddleware));
 
